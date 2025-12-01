8aa05a05f2808af8ad37b9657409a9e89b3b364b
Jack Bergin <jackcbergin@gmail.com>
2025-12-01 16:32:26 -0500

    Made this file to singleton the load_dotenv

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+# Configuration
+API_URL = 'http://localhost:8081/measurements'
+SENSORS = ['sensor_001', 'sensor_002', 'sensor_003']
+INTERVAL_SECONDS = 0.5
+
+# Realistic value ranges for water quality monitoring
+TEMP_RANGE = (20.0, 30.0) # Celsius
+CONDUCTIVITY_RANGE = (1000, 3000) # microsiemens/cm
